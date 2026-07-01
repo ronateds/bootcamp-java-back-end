8ab30a92de8dd44b537e34250f83076435edf685
Ronate dos Santos <38088684+ronateds@users.noreply.github.com>
2026-07-01 05:14:54 -0300

    converted into compact source file as suggested by the IDE.

--- src/Exercicio01.java
+++ src/Exercicio01.java
@@ -1,23 +1,19 @@
-import java.util.Scanner;
+void main() {
+    mensagem();
+}
 
-public class Exercicio01 {
-    public static void main(String[] args) {
-        mensagem();
-    }
+/**
+ *  1. Escreva um código que receba o nome e o ano de nascimento de alguém
+ *  e imprima na tela a seguinte mensagem: "Olá 'Fulano' você tem 'X' anos"
+ */
+public static void mensagem() {
+    Scanner scanner = new Scanner(System.in);
 
-    /**
-     *  1. Escreva um código que receba o nome e o ano de nascimento de alguém
-     *  e imprima na tela a seguinte mensagem: "Olá 'Fulano' você tem 'X' anos"
-     */
-    public static void mensagem() {
-        Scanner scanner = new Scanner(System.in);
+    IO.print("Digite o seu nome: ");
+    String name = scanner.next();
 
-        System.out.print("Digite o seu nome: ");
-        String name = scanner.next();
+    IO.print("Digite sua idade: ");
+    byte age = scanner.nextByte();
 
-        System.out.print("Digite sua idade: ");
-        byte age = scanner.nextByte();
-
-        System.out.printf("Olá %s você tem %s anos", name, age);
-    }
+    System.out.printf("Olá %s você tem %s anos", name, age);
 }
